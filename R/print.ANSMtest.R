3730990b4396e6213f90ad0af35b853fb016e3c1
Neil Spencer <neilhspencer@gmail.com>
2022-12-13 17:48:35 +0000

    Update to allow for printing of exact and approximate p-values.

--- R/print.ANSMtest.R
+++ R/print.ANSMtest.R
@@ -1,26 +1,55 @@
 print.ANSMtest <- function(toprint) {
   cat("\n")
+
   cat(paste0(toprint$title, " for ", toprint$varname))
+
   cat("\n\n")
+
   cat(paste0("Null hypothesis: theta = ", toprint$H0))
+
   cat("\n")
+
   if (toprint$alternative == "two.sided") {
-    cat(paste0("Alternative hypothesis (2-sided): theta <> ", toprint$H0))
+    cat(paste0("Alternative hypothesis (2-sided): theta <> ", toprint$H0), "\n")
   }else if (toprint$alternative == "less") {
-    cat(paste0("Alternative hypothesis (1-sided): theta < ", toprint$H0))
+    cat(paste0("Alternative hypothesis (1-sided): theta < ", toprint$H0), "\n")
   }else{
-    cat(paste0("Alternative hypothesis (1-sided): theta > ", toprint$H0))
+    cat(paste0("Alternative hypothesis (1-sided): theta > ", toprint$H0), "\n")
   }
+
+  if (!is.null(toprint$pval)){
+    if (round(toprint$pval, 5) == 0){
+      cat("p-value: < 0.00001\n")
+    }else{
+      cat(paste0("p-value: ", round(toprint$pval,5), "\n"))
+    }
+  }
+  if (!is.null(toprint$pval.approx)){
+    if (toprint$cont.corr == TRUE){
+      pval.approx.label = "Approx. p-value (continuity correction used): "
+    }else{
+      pval.approx.label = "Approx. p-value (continuity correction not used): "
+    }
+    if (round(toprint$pval.approx, 5) == 0){
+      cat(paste0(pval.approx.label, "< 0.00001\n"))
+    }else{
+      cat(paste0(pval.approx.label, round(toprint$pval.approx,5), "\n"))
+    }
+    if (!is.null(toprint$pval.exact)){
+      if (round(toprint$pval.exact, 5) == 0){
+        cat("Exact p-value: < 0.00001\n")
+      }else{
+        cat(paste0("Exact p-value: ", round(toprint$pval.exact,5), "\n"))
+      }
+    }
+  }
+
   cat("\n")
-  if (round(toprint$pval, 5) == 0){
-    cat(paste0("Exact p-value: ", toprint$pval))
-  }else{
-    cat(paste0("Exact p-value: ", round(toprint$pval,5)))
-  }
-  cat("\n\n")
+
   cat(paste0(100 * round(toprint$targetCIwidth, 5), "% Confidence Interval (",
-             100 * round(toprint$actualCIwidth, 5), "% achieved)"))
+             100 * round(toprint$actualCIwidth, 5), "% achieved)"), "\n")
+
+  cat(paste0("(", toprint$CI.lower, ", ", toprint$CI.upper, ")"), "\n")
+
   cat("\n")
-  cat(paste0("(", toprint$CI.lower, ", ", toprint$CI.upper, ")"))
-  cat("\n\n")
 }
