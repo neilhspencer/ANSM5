f47fb79166ac1cd33533dd7b84b9e1b487a51bf3
Neil Spencer <neilhspencer@gmail.com>
2022-12-14 16:59:51 +0000

    New function created

---
+++ R/wilcoxon.signed.rank.test.R
@@ -0,0 +1,144 @@
+wilcoxon.signed.rank.test <-
+  function(x, H0, alternative=c("two.sided", "less", "greater"),
+           cont.corr = TRUE, CI.width = 0.95, do.approx = TRUE, do.exact = TRUE,
+           do.CI = TRUE) {
+  stopifnot(is.vector(x), is.numeric(x), is.numeric(H0), length(H0) == 1,
+            is.logical(cont.corr) == TRUE, CI.width > 0, CI.width < 1,
+            is.logical(do.approx) == TRUE, is.logical(do.exact) == TRUE,
+            is.logical(do.CI) == TRUE)
+  alternative <- match.arg(alternative)
+
+  #labels
+  varname <- deparse(substitute(x))
+
+  #defaults
+  pval <- NULL
+  pval.stat <- NULL
+  pval.note <- NULL
+  pval.approx <- NULL
+  pval.approx.stat <- NULL
+  pval.approx.note <- NULL
+  pval.exact <- NULL
+  pval.exact.stat <- NULL
+  pval.exact.note <- NULL
+  actualCIwidth <- NULL
+  CI.lower <- NULL
+  CI.upper <- NULL
+  CI.note <- NULL
+  test.note <- NULL
+
+  #statistics
+  x <- x[complete.cases(x)] #remove missing cases
+  x <- x[x != H0] #remove cases equal to H0
+  ranksumplus <- sum(rank(abs(x - H0))[sign(x - H0) == 1])
+  ranksumminus <- sum(rank(abs(x - H0))[sign(x - H0) == -1])
+  n <- length(x)
+  perms <- eval(parse(text = paste0("expand.grid(", strrep("c(-1, 1),", n - 1),
+                                    "c(-1, 1))")))
+  perms = perms * matrix(rep(seq(1, n), rep(2 ^ n)), nrow = 2 ^ n, ncol = n,
+                         byrow=TRUE)
+  permsplus <- data.frame(table(rowSums(replace(perms, perms < 0, 0))))
+  permsplus$Var1 <- strtoi(permsplus$Var1)
+  permsminus <- data.frame(table(rowSums(replace(perms, perms > 0, 0))))
+  permsminus$Var1 <- -strtoi(permsminus$Var1)
+
+  #approx p-value (with/without continuity correction)
+  if (do.approx){
+    S <- min(ranksumminus, ranksumplus)
+    pval.approx.stat <- ((S + 0.5 * (cont.corr == TRUE)) - n * (n + 1) / 4) /
+      sqrt(n * (n + 1) * (2 * n + 1) / 24)
+    pval.approx.less <-
+      pnorm(((ranksumminus + (0.5 - (ranksumminus > ranksumplus)) *
+                (cont.corr == TRUE)) - n * (n + 1) / 4) /
+              sqrt(n * (n + 1) * (2 * n + 1) / 24), lower.tail = FALSE)
+    pval.approx.greater <-
+      pnorm(((ranksumplus + (0.5 - (ranksumminus < ranksumplus)) *
+                (cont.corr == TRUE)) - n * (n + 1) / 4) /
+              sqrt(n * (n + 1) * (2 * n + 1) / 24), lower.tail = FALSE)
+    if (alternative=="two.sided"){
+      pval.approx <- min(pval.approx.less, pval.approx.greater) * 2
+    }else if (alternative == "less"){
+      pval.approx <- pval.approx.less
+    }else if (alternative == "greater"){
+      pval.approx <- pval.approx.greater
+    }
+    if (n < 12){
+      pval.approx.note <-
+        pval.approx.note <- paste("(WARNING: n is less than 12 so",
+        "approximation is not recommended)")
+    }
+  }
+
+  #exact p-value
+  if (do.exact && max(table(x)) == 1){
+    pval.exact.stat <- min(ranksumminus, ranksumplus)
+    pval.exact.less <-
+      sum(permsminus[permsminus$Var1 >= ranksumminus, "Freq"]) /
+      sum(permsminus$Freq)
+    pval.exact.greater <-
+      sum(permsplus[permsplus$Var1 >= ranksumplus, "Freq"]) /
+      sum(permsplus$Freq)
+    if (alternative=="two.sided"){
+      pval.exact <- min(pval.exact.less, pval.exact.greater) * 2
+    }else if (alternative == "less"){
+      pval.exact <- pval.exact.less
+    }else if (alternative == "greater"){
+      pval.exact <- pval.exact.greater
+    }
+  }
+
+  #CI
+  if (do.CI){
+    Walsh.averages <- NULL
+    for (i in 1:length(x)){
+      for (j in i:length(x)){
+        Walsh.averages <- c(Walsh.averages, (x[i] + x[j]) / 2)
+      }
+    }
+    i <- 0
+    repeat{
+      pval.tmp <-
+        sum(permsplus[permsplus$Var1 <= i, "Freq"]) / sum(permsplus$Freq) * 2
+      if (pval.tmp > (1 - CI.width)) {break}
+      i <- i + 1
+    }
+    CI.lower <- sort(Walsh.averages, decreasing = FALSE)[i]
+    CI.upper <- sort(Walsh.averages, decreasing = TRUE)[i]
+    actualCIwidth <- 1 - sum(permsplus[permsplus$Var1 <= i - 1, "Freq"]) /
+      sum(permsplus$Freq) * 2
+    if (is.null(CI.lower) | is.null(CI.upper) | is.null(actualCIwidth)){
+      actualCIwidth <- NULL
+      CI.lower <- NULL
+      CI.upper <- NULL
+    }
+  }
+
+  #check if anything done
+  if (!do.approx && !do.exact && !do.CI) {
+    test.note <- paste("Neither exact test, nor approximation test nor",
+                       "confidence interval requested")
+  }else if ((do.approx | do.CI) && do.exact && max(table(x)) > 1) {
+    test.note <- paste("NOTE: Ties exist in the data being used so exact test",
+                       "not calculated\nand mid-ranks have been used for the",
+                       "approximation test")
+  }else if (do.exact && max(table(x)) > 1) {
+    test.note <- paste("NOTE: Ties exist in the data being used so exact test",
+                       "not calculated")
+  }else if (max(table(x)) > 1) {
+    test.note <- paste("NOTE: Ties exist in the data being used and mid-ranks",
+                       "have been calculated")
+  }
+
+  #return
+  result <- list(title = "Wilcoxon signed-rank test", varname = varname,
+                 H0 = H0, alternative = alternative, cont.corr = cont.corr,
+                 pval = pval, pval.stat = pval.stat, pval.note = pval.note,
+                 pval.approx = pval.approx, pval.approx.stat = pval.approx.stat,
+                 pval.approx.note = pval.approx.note, pval.exact = pval.exact,
+                 pval.exact.stat = pval.exact.stat,
+                 pval.exact.note = pval.exact.note, targetCIwidth = CI.width,
+                 actualCIwidth = actualCIwidth, CI.lower = CI.lower,
+                 CI.upper = CI.upper, CI.note = CI.note, test.note = test.note)
+  class(result) <- "ANSMtest"
+  return(result)
+}
---
+++ tests/testthat/test-wilcoxon.signed.R
@@ -0,0 +1,20 @@
+test_that("exact p-val works", {
+  heartrates1 <- c(73, 82, 87, 68, 106, 60, 97)
+  heartrates2 <- c(-2,4,8,25,-5,16,3,1,12,17,20,9)
+  withties <- c(4, 4, 8, 8, 11, 11, 11)
+  ages <- c(6, 18, 24, 26, 37, 40, 42, 47, 69, 69, 87, 130)
+  expect_equal(wilcoxon.signed.rank.test(heartrates1, 70,
+                                         "greater")$pval.exact.stat, 4)
+  expect_equal(wilcoxon.signed.rank.test(heartrates1, 70,
+                                         "greater")$pval.exact, 0.05469)
+  expect_equal(wilcoxon.signed.rank.test(heartrates2, 15,
+                                         "less")$pval.exact.stat, 14)
+  expect_equal(wilcoxon.signed.rank.test(heartrates2, 15,
+                                         "less")$pval.exact, 0.02612)
+  expect_equal(wilcoxon.signed.rank.test(heartrates2, 15)$pval.exact.stat, 14)
+  expect_equal(wilcoxon.signed.rank.test(heartrates2, 15)$pval.exact, 0.05225)
+  expect_equal(wilcoxon.signed.rank.test(withties, 6, "greater")$pval.exact.stat, 5)
+  expect_equal(wilcoxon.signed.rank.test(withties, 6, "greater")$pval.exact, 0.07812)
+  expect_equal(wilcoxon.signed.rank.test(ages, 30, "greater")$pval.exact.stat, 5)
+  expect_equal(wilcoxon.signed.rank.test(ages, 30, "greater")$pval.exact, 0.07812)
+})
