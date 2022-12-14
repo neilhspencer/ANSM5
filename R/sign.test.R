f99acdc34b2e6addcf86c820bfd54a885fe3b11f
Neil Spencer <neilhspencer@gmail.com>
2022-12-14 16:59:26 +0000

    Updates with additional options and addition of comments

--- R/sign.test.R
+++ R/sign.test.R
@@ -1,69 +1,117 @@
-sign.test <- function(x, H0, alternative=c("two.sided", "less", "greater"), cont.corr = TRUE, CI.width = 0.95) {
-  stopifnot(is.vector(x), is.numeric(x), is.numeric(H0), length(H0) == 1, is.logical(cont.corr) == TRUE,
-            CI.width > 0, CI.width < 1)
+sign.test <- function(x, H0, alternative=c("two.sided", "less", "greater"),
+                      cont.corr = TRUE, CI.width = 0.95, do.approx = TRUE,
+                      do.exact = TRUE, do.CI = TRUE) {
+  stopifnot(is.vector(x), is.numeric(x), is.numeric(H0), length(H0) == 1,
+            is.logical(cont.corr) == TRUE, CI.width > 0, CI.width < 1,
+            is.logical(do.approx) == TRUE, is.logical(do.exact) == TRUE,
+            is.logical(do.CI) == TRUE)
   alternative <- match.arg(alternative)
+
+  #labels
+  varname <- deparse(substitute(x))
+
+  #defaults
+  pval <- NULL
+  pval.stat <- NULL
+  pval.note <- NULL
+  pval.approx <- NULL
+  pval.approx.stat <- NULL
+  pval.approx.note <- NULL
+  pval.exact <- NULL
+  pval.exact.stat <- NULL
+  pval.exact.note <- NULL
+  actualCIwidth <- NULL
+  CI.lower <- NULL
+  CI.upper <- NULL
+  CI.note <- NULL
+  test.note <- NULL
 
   #statistics
   pluses <- sum(x > H0, na.rm = TRUE)
   n <- sum(x != H0, na.rm = TRUE)
 
-  #exact p-value
-  if (alternative=="two.sided"){
-    pval.exact<-pbinom(pluses, n, 0.5)+(1-pbinom(n-pluses-1, n, 0.5))
-  }else if (alternative == "less"){
-    pval.exact <- pbinom(pluses, n, 0.5)
-  }else if (alternative == "greater"){
-    pval.exact <- pbinom(n-pluses-1, n, 0.5)
+  #approx p-value (with/without continuity correction)
+  if (do.approx){
+    if (alternative=="two.sided"){
+      pval.approx <- pnorm((pluses - n * 0.5 + (0.5 - (pluses > n * 0.5)) *
+                              (cont.corr == TRUE))/(0.5 * sqrt(n)),
+                           lower.tail = pluses < n * 0.5) * 2
+    }else if (alternative == "less"){
+      pval.approx <- pnorm((pluses - n * 0.5 + (0.5 - (pluses > n * 0.5)) *
+                              (cont.corr == TRUE))/(0.5 * sqrt(n)),
+                           lower.tail = TRUE)
+    }else if (alternative == "greater"){
+      pval.approx <- pnorm((pluses - n * 0.5 + (0.5 - (pluses > n * 0.5)) *
+                              (cont.corr == TRUE))/(0.5 * sqrt(n)),
+                           lower.tail = FALSE)
+    }
+    if (pluses < 5 | n - pluses < 5){
+      pval.approx.note <- paste("(WARNING: n * p and/or n * (1 - p) are less",
+                                "than 5 so approximation is not recommended)")
+    }
   }
 
-  #approx p-value (with/without continuity correction)
-  if (alternative=="two.sided"){
-    pval.approx<-pnorm((pluses - n * 0.5 + (0.5 - (pluses > n * 0.5)) * (cont.corr == TRUE))/(0.5 * sqrt(n)),
-                       lower.tail = pluses < n * 0.5) * 2
-  }else if (alternative == "less"){
-    pval.approx <- pnorm((pluses - n * 0.5 + (0.5 - (pluses > n * 0.5)) * (cont.corr == TRUE))/(0.5 * sqrt(n)),
-                         lower.tail = TRUE)
-  }else if (alternative == "greater"){
-    pval.approx <- pnorm((pluses - n * 0.5 + (0.5 - (pluses > n * 0.5)) * (cont.corr == TRUE))/(0.5 * sqrt(n)),
-                         lower.tail = FALSE)
-  }else{
-    pval.approx <- NULL
+  #exact p-value
+  if (do.exact){
+    if (alternative=="two.sided"){
+      pval.exact <- pbinom(pluses, n, 0.5)+(1-pbinom(n-pluses-1, n, 0.5))
+    }else if (alternative == "less"){
+      pval.exact <- pbinom(pluses, n, 0.5)
+    }else if (alternative == "greater"){
+      pval.exact <- pbinom(n-pluses-1, n, 0.5)
+    }
   }
 
   #CI
-  lower <- 0
-  lower.cumu <- 0
-  repeat{
-    if (lower.cumu + dbinom(lower, n, 0.5) > (1-CI.width)/2) {break}
-    lower.cumu <- lower.cumu + dbinom(lower, n, 0.5)
-    lower <- lower + 1
-  }
-  if (lower == 0){
-    CI.lower = Inf
-  }else{
-    CI.lower <- sort(x)[lower]
-  }
-  upper <- n
-  upper.cumu <- 0
-  repeat{
-    if (upper.cumu + dbinom(upper, n, 0.5) > (1-CI.width)/2) {break}
-    upper.cumu <- upper.cumu + dbinom(upper, n, 0.5)
-    upper <- upper - 1
-  }
-  if (upper == n){
-    CI.upper = Inf
-  }else{
-    CI.upper <- sort(x)[upper+1]
+  if (do.CI){
+    lower <- 0
+    lower.cumu <- 0
+    repeat{
+      if (lower.cumu + dbinom(lower, n, 0.5) > (1-CI.width)/2) {break}
+      lower.cumu <- lower.cumu + dbinom(lower, n, 0.5)
+      lower <- lower + 1
+    }
+    if (lower == 0){
+      CI.lower = Inf
+    }else{
+      CI.lower <- sort(x)[lower]
+    }
+    upper <- n
+    upper.cumu <- 0
+    repeat{
+      if (upper.cumu + dbinom(upper, n, 0.5) > (1-CI.width)/2) {break}
+      upper.cumu <- upper.cumu + dbinom(upper, n, 0.5)
+      upper <- upper - 1
+    }
+    if (upper == n){
+      CI.upper = Inf
+    }else{
+      CI.upper <- sort(x)[upper+1]
+    }
+    actualCIwidth <- 1 - lower.cumu - upper.cumu
+    if (is.null(CI.lower) | is.null(CI.upper) | is.null(actualCIwidth)){
+      actualCIwidth <- NULL
+      CI.lower <- NULL
+      CI.upper <- NULL
+    }
   }
 
-  #labels
-  varname <- deparse(substitute(x))
-  actualCIwidth <- 1 - lower.cumu - upper.cumu
+  #check if anything done
+  if (!do.approx && !do.exact && !do.CI) {
+    test.note <- paste("Neither exact test, nor approximation test nor",
+                       "confidence interval requested")
+  }
 
   #return
-  result <- list(title = "Sign test", varname = varname, H0 = H0, alternative = alternative, cont.corr = cont.corr,
-                 pval = NULL, pval.exact = pval.exact, pval.approx = pval.approx, targetCIwidth = CI.width,
-                 actualCIwidth = actualCIwidth, CI.lower = CI.lower, CI.upper = CI.upper)
+  result <- list(title = "Sign test", varname = varname, H0 = H0,
+                 alternative = alternative, cont.corr = cont.corr, pval = pval,
+                 pval.stat = pval.stat, pval.note = pval.note,
+                 pval.approx = pval.approx, pval.approx.stat = pval.approx.stat,
+                 pval.approx.note = pval.approx.note, pval.exact = pval.exact,
+                 pval.exact.stat = pval.exact.stat,
+                 pval.exact.note = pval.exact.note, targetCIwidth = CI.width,
+                 actualCIwidth = actualCIwidth, CI.lower = CI.lower,
+                 CI.upper = CI.upper, CI.note = CI.note, test.note = test.note)
   class(result) <- "ANSMtest"
   return(result)
 }
--- tests/testthat/test-sign.R
+++ tests/testthat/test-sign.R
@@ -1,22 +1,22 @@
 test_that("exact p-val works", {
   sampleI <- c(1, 11, 35, 41, 50, 62, 104, 104, 151, 161)
   sampleII <- c(2, 10, 27, 35, 38, 40, 47, 50, 58, 60, 68, 70, 79, 103, 108, 151, 157, 158, 162, 166)
-  expect_equal(sign.test(sampleI,110)$pval.exact, 0.109375)
-  expect_equal(sign.test(sampleII,110)$pval.exact, 0.041389465)
+  expect_equal(sign.test(sampleI, 110)$pval.exact, 0.109375)
+  expect_equal(sign.test(sampleII, 110)$pval.exact, 0.041389465)
 })
 
 test_that("CI works", {
   sampleI <- c(1, 11, 35, 41, 50, 62, 104, 104, 151, 161)
   sampleII <- c(2, 10, 27, 35, 38, 40, 47, 50, 58, 60, 68, 70, 79, 103, 108, 151, 157, 158, 162, 166)
-  expect_equal(sign.test(sampleI,110)$CI.lower, 11)
-  expect_equal(sign.test(sampleI,110)$CI.upper, 151)
-  expect_equal(sign.test(sampleII,110)$CI.lower, 40)
-  expect_equal(sign.test(sampleII,110)$CI.upper, 108)
+  expect_equal(sign.test(sampleI, 110)$CI.lower, 11)
+  expect_equal(sign.test(sampleI, 110)$CI.upper, 151)
+  expect_equal(sign.test(sampleII, 110)$CI.lower, 40)
+  expect_equal(sign.test(sampleII, 110)$CI.upper, 108)
 })
 
 test_that("actualCIwidth works", {
   sampleI <- c(1, 11, 35, 41, 50, 62, 104, 104, 151, 161)
   sampleII <- c(2, 10, 27, 35, 38, 40, 47, 50, 58, 60, 68, 70, 79, 103, 108, 151, 157, 158, 162, 166)
-  expect_equal(sign.test(sampleI,110)$actualCIwidth, 0.97851562)
-  expect_equal(sign.test(sampleII,110)$actualCIwidth, 0.95861053)
+  expect_equal(sign.test(sampleI, 110)$actualCIwidth, 0.97851562)
+  expect_equal(sign.test(sampleII, 110)$actualCIwidth, 0.95861053)
 })
