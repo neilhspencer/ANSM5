949340cfe28c616a0d528c45f0c557e0bcc78e30
Neil Spencer <neilhspencer@gmail.com>
2022-12-16 17:10:24 +0000

    Changing "approximate" to "asymptotic"

--- R/sign.test.R
+++ R/sign.test.R
@@ -1,11 +1,11 @@
 sign.test <- function(x, H0, alternative=c("two.sided", "less", "greater"),
                       cont.corr = TRUE, CI.width = 0.95,
-                      max.exact.cases = 1000000, do.approx = TRUE,
+                      max.exact.cases = 1000000, do.asymp = TRUE,
                       do.exact = TRUE, do.CI = TRUE) {
   stopifnot(is.vector(x), is.numeric(x), is.numeric(H0), length(H0) == 1,
             is.numeric(max.exact.cases), length(max.exact.cases) == 1,
             is.logical(cont.corr) == TRUE, CI.width > 0, CI.width < 1,
-            is.logical(do.approx) == TRUE, is.logical(do.exact) == TRUE,
+            is.logical(do.asymp) == TRUE, is.logical(do.exact) == TRUE,
             is.logical(do.CI) == TRUE)
   alternative <- match.arg(alternative)
 
@@ -16,9 +16,9 @@ sign.test <- function(x, H0, alternative=c("two.sided", "less", "greater"),
   pval <- NULL
   pval.stat <- NULL
   pval.note <- NULL
-  pval.approx <- NULL
-  pval.approx.stat <- NULL
-  pval.approx.note <- NULL
+  pval.asymp <- NULL
+  pval.asymp.stat <- NULL
+  pval.asymp.note <- NULL
   pval.exact <- NULL
   pval.exact.stat <- NULL
   pval.exact.note <- NULL
@@ -31,27 +31,27 @@ sign.test <- function(x, H0, alternative=c("two.sided", "less", "greater"),
   #statistics
   x <- x[complete.cases(x)] #remove missing cases
   x <- x[x != H0] #remove cases equal to H0
-  pluses <- sum(x > H0)
   n <- length(x)
+  pluses <- sum(x > H0)
 
-  #approx p-value (with/without continuity correction)
-  if (do.approx){
+  #asymptotic p-value (with/without continuity correction)
+  if (do.asymp){
     if (alternative=="two.sided"){
-      pval.approx <- pnorm((pluses - n * 0.5 + (0.5 - (pluses > n * 0.5)) *
+      pval.asymp <- pnorm((pluses - n * 0.5 + (0.5 - (pluses > n * 0.5)) *
                               (cont.corr == TRUE))/(0.5 * sqrt(n)),
                            lower.tail = pluses < n * 0.5) * 2
     }else if (alternative == "less"){
-      pval.approx <- pnorm((pluses - n * 0.5 + (0.5 - (pluses > n * 0.5)) *
+      pval.asymp <- pnorm((pluses - n * 0.5 + (0.5 - (pluses > n * 0.5)) *
                               (cont.corr == TRUE))/(0.5 * sqrt(n)),
                            lower.tail = TRUE)
     }else if (alternative == "greater"){
-      pval.approx <- pnorm((pluses - n * 0.5 + (0.5 - (pluses > n * 0.5)) *
+      pval.asymp <- pnorm((pluses - n * 0.5 + (0.5 - (pluses > n * 0.5)) *
                               (cont.corr == TRUE))/(0.5 * sqrt(n)),
                            lower.tail = FALSE)
     }
     if (pluses < 5 | n - pluses < 5){
-      pval.approx.note <- paste("(WARNING: n * p and/or n * (1 - p) are less",
-                                "than 5 so approximation is not recommended)")
+      pval.asymp.note <- paste("(WARNING: n * p and/or n * (1 - p) are less",
+                                "than 5 so asymptotic test is not recommended)")
     }
   }
 
@@ -103,22 +103,22 @@ sign.test <- function(x, H0, alternative=c("two.sided", "less", "greater"),
   }
 
   #check if message needed
-  if (!do.approx && !do.exact && !do.CI) {
-    test.note <- paste("Neither exact test, nor approximation test nor",
+  if (!do.asymp && !do.exact && !do.CI) {
+    test.note <- paste("Neither exact test, nor asymptotic test nor",
                        "confidence interval requested")
   }else if ((do.CI | do.exact) && n > max.exact.cases) {
     test.note <- paste0("NOTE: Number of useful cases greater than current ",
                         "maximum allowed for exact calculations\nrequired for ",
-                        "exact test or confidence interval (", max.exact.cases,
-                        ")")
+                        "exact test or confidence interval (max.exact.cases = ",
+                        max.exact.cases, ")")
   }
 
   #return
   result <- list(title = "Sign test", varname = varname, H0 = H0,
                  alternative = alternative, cont.corr = cont.corr, pval = pval,
                  pval.stat = pval.stat, pval.note = pval.note,
-                 pval.approx = pval.approx, pval.approx.stat = pval.approx.stat,
-                 pval.approx.note = pval.approx.note, pval.exact = pval.exact,
+                 pval.asymp = pval.asymp, pval.asymp.stat = pval.asymp.stat,
+                 pval.asymp.note = pval.asymp.note, pval.exact = pval.exact,
                  pval.exact.stat = pval.exact.stat,
                  pval.exact.note = pval.exact.note, targetCIwidth = CI.width,
                  actualCIwidth = actualCIwidth, CI.lower = CI.lower,
