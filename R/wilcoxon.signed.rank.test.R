aad7c4457c96c38b2071a4d8164d568675819bf7
Neil Spencer <neilhspencer@gmail.com>
2022-12-16 17:12:29 +0000

    Change "approximate" to "asymptotic", add code for asymptotic test with tied ranks and amend warning for exceeding maximum number of cases for exact calculations to include name of parameter

--- R/wilcoxon.signed.rank.test.R
+++ R/wilcoxon.signed.rank.test.R
@@ -1,10 +1,10 @@
 wilcoxon.signed.rank.test <-
   function(x, H0, alternative=c("two.sided", "less", "greater"),
            cont.corr = TRUE, CI.width = 0.95, max.exact.cases = 1000,
-           do.approx = TRUE, do.exact = TRUE, do.CI = TRUE) {
+           do.asymp = TRUE, do.exact = TRUE, do.CI = TRUE) {
   stopifnot(is.vector(x), is.numeric(x), is.numeric(H0), length(H0) == 1,
             is.logical(cont.corr) == TRUE, CI.width > 0, CI.width < 1,
-            is.logical(do.approx) == TRUE, is.logical(do.exact) == TRUE,
+            is.logical(do.asymp) == TRUE, is.logical(do.exact) == TRUE,
             is.logical(do.CI) == TRUE)
   alternative <- match.arg(alternative)
 
@@ -15,9 +15,9 @@ wilcoxon.signed.rank.test <-
   pval <- NULL
   pval.stat <- NULL
   pval.note <- NULL
-  pval.approx <- NULL
-  pval.approx.stat <- NULL
-  pval.approx.note <- NULL
+  pval.asymp <- NULL
+  pval.asymp.stat <- NULL
+  pval.asymp.note <- NULL
   pval.exact <- NULL
   pval.exact.stat <- NULL
   pval.exact.note <- NULL
@@ -30,9 +30,10 @@ wilcoxon.signed.rank.test <-
   #statistics
   x <- x[complete.cases(x)] #remove missing cases
   x <- x[x != H0] #remove cases equal to H0
-  ranksumplus <- sum(rank(abs(x - H0))[sign(x - H0) == 1])
-  ranksumminus <- sum(rank(abs(x - H0))[sign(x - H0) == -1])
   n <- length(x)
+  s <- rank(abs(x - H0))
+  ranksumplus <- sum(s[sign(x - H0) == 1])
+  ranksumminus <- sum(s[sign(x - H0) == -1])
   if (n < max.exact.cases){
     permsums <- rep(0,sum(seq(1, n)) + 1)
     permsums[1] <- 1
@@ -40,37 +41,48 @@ wilcoxon.signed.rank.test <-
       permsumsnow <- permsums
       for (j in 1:length(permsums)){
         if (permsumsnow[j] > 0){
-          permsums[(j - 1) + i + 1] <- permsums[(j - 1) + i + 1] + permsumsnow[j]
+          permsums[(j - 1) + i + 1] <-
+            permsums[(j - 1) + i + 1] + permsumsnow[j]
         }
       }
     }
     permsums <- data.frame(0:(length(permsums) - 1), permsums)
   }
 
-  #approx p-value (with/without continuity correction)
-  if (do.approx){
+  #asymptotic p-value (with/without continuity correction)
+  if (do.asymp){
     S <- min(ranksumminus, ranksumplus)
-    pval.approx.stat <- ((S + 0.5 * (cont.corr == TRUE)) - n * (n + 1) / 4) /
-      sqrt(n * (n + 1) * (2 * n + 1) / 24)
-    pval.approx.less <-
-      pnorm(((ranksumminus + (0.5 - (ranksumminus > ranksumplus)) *
-                (cont.corr == TRUE)) - n * (n + 1) / 4) /
-              sqrt(n * (n + 1) * (2 * n + 1) / 24), lower.tail = FALSE)
-    pval.approx.greater <-
-      pnorm(((ranksumplus + (0.5 - (ranksumminus < ranksumplus)) *
-                (cont.corr == TRUE)) - n * (n + 1) / 4) /
-              sqrt(n * (n + 1) * (2 * n + 1) / 24), lower.tail = FALSE)
-    if (alternative=="two.sided"){
-      pval.approx <- min(pval.approx.less, pval.approx.greater) * 2
-    }else if (alternative == "less"){
-      pval.approx <- pval.approx.less
-    }else if (alternative == "greater"){
-      pval.approx <- pval.approx.greater
+    if (max(table(x)) > 1){
+      pval.asymp.stat <- (S - 0.5 * sum(abs(s))) / (0.5 * sqrt(sum(s ** 2)))
+      pval.asymp.less <-
+        pnorm((ranksumminus - 0.5 * sum(abs(s))) / (0.5 * sqrt(sum(s ** 2))),
+              lower.tail = FALSE)
+      pval.asymp.greater <-
+        pnorm((ranksumplus - 0.5 * sum(abs(s))) / (0.5 * sqrt(sum(s ** 2))),
+              lower.tail = FALSE)
+    }else{
+      pval.asymp.stat <- ((S + 0.5 * (cont.corr == TRUE)) - n * (n + 1) / 4) /
+        sqrt(n * (n + 1) * (2 * n + 1) / 24)
+      pval.asymp.less <-
+        pnorm(((ranksumminus + (0.5 - (ranksumminus > ranksumplus)) *
+                  (cont.corr == TRUE)) - n * (n + 1) / 4) /
+                sqrt(n * (n + 1) * (2 * n + 1) / 24), lower.tail = FALSE)
+      pval.asymp.greater <-
+        pnorm(((ranksumplus + (0.5 - (ranksumminus < ranksumplus)) *
+                  (cont.corr == TRUE)) - n * (n + 1) / 4) /
+                sqrt(n * (n + 1) * (2 * n + 1) / 24), lower.tail = FALSE)
     }
-    if (n < 12){
-      pval.approx.note <-
-        pval.approx.note <- paste("(WARNING: n is less than 12 so",
-        "approximation is not recommended)")
+    if (alternative=="two.sided"){
+      pval.asymp <- min(pval.asymp.less, pval.asymp.greater) * 2
+    }else if (alternative == "less"){
+      pval.asymp <- pval.asymp.less
+    }else if (alternative == "greater"){
+      pval.asymp <- pval.asymp.greater
+    }
+    if (n < 20){
+      pval.asymp.note <-
+        pval.asymp.note <- paste("(WARNING: n is less than 20 so asymptotic",
+                                 "test is not recommended)")
     }
   }
 
@@ -80,7 +92,7 @@ wilcoxon.signed.rank.test <-
     pval.exact.less <-
       sum(permsums[permsums[, 1] >= ranksumminus, 2]) / sum(permsums[, 2])
     pval.exact.greater <-
-      sum(permsums[permsplus[, 1] >= ranksumplus, 2]) / sum(permsplus[, 2])
+      sum(permsums[permsums[, 1] >= ranksumplus, 2]) / sum(permsums[, 2])
     if (alternative=="two.sided"){
       pval.exact <- min(pval.exact.less, pval.exact.greater) * 2
     }else if (alternative == "less"){
@@ -101,14 +113,14 @@ wilcoxon.signed.rank.test <-
     i <- 0
     repeat{
       pval.tmp <-
-        sum(permsplus[permsplus[, 1] <= i, 2]) / sum(permsplus[, 2]) * 2
+        sum(permsums[permsums[, 1] <= i, 2]) / sum(permsums[, 2]) * 2
       if (pval.tmp > (1 - CI.width)) {break}
       i <- i + 1
     }
     CI.lower <- sort(Walsh.averages, decreasing = FALSE)[i]
     CI.upper <- sort(Walsh.averages, decreasing = TRUE)[i]
-    actualCIwidth <- 1 - sum(permsplus[permsplus[, 1] <= i - 1, 2]) /
-      sum(permsplus[, 2]) * 2
+    actualCIwidth <- 1 - sum(permsums[permsums[, 1] <= i - 1, 2]) /
+      sum(permsums[, 2]) * 2
     if (is.null(CI.lower) | is.null(CI.upper) | is.null(actualCIwidth)){
       actualCIwidth <- NULL
       CI.lower <- NULL
@@ -117,8 +129,8 @@ wilcoxon.signed.rank.test <-
   }
 
   #check if message needed
-  if (!do.approx && !do.exact && !do.CI) {
-    test.note <- paste("Neither exact test, nor approximation test nor",
+  if (!do.asymp && !do.exact && !do.CI) {
+    test.note <- paste("Neither exact test, nor asymptotic test nor",
                        "confidence interval requested")
   }else if (max(table(x)) > 1){
     affected <- NULL
@@ -129,16 +141,15 @@ wilcoxon.signed.rank.test <-
     }else if (do.CI){
       affected <- "confidence interval"
     }
-    if (do.approx && !is.null(affected)){
+    if (do.asymp && !is.null(affected)){
       test.note <- paste("NOTE: Ties exist in data so", affected, "not",
-                          "calculated\nand mid-ranks used for approximation",
-                          "test")
-    }else if (!do.approx && !is.null(affected)){
+                          "calculated\nand mid-ranks used for asymptotic test")
+    }else if (!do.asymp && !is.null(affected)){
       test.note <- paste("NOTE: Ties exist in data so", affected, "not",
                           "calculated")
-    }else if (do.approx && is.null (affected)){
+    }else if (do.asymp && is.null (affected)){
       test.note <- paste("NOTE: Ties exist in data so mid-ranks used for",
-                          "approximation test")
+                          "asymptotic test")
     }
   }else if (n > max.exact.cases) {
     affected <- NULL
@@ -152,7 +163,8 @@ wilcoxon.signed.rank.test <-
     if (!is.null(affected)){
       test.note <- paste0("NOTE: Number of useful cases greater than current ",
                            "maximum allowed for exact\ncalculations required ",
-                           "for ", affected, " (", max.exact.cases, ")")
+                           "for ", affected, " (max.exact.cases = ",
+                          max.exact.cases, ")")
     }
   }
 
@@ -160,8 +172,8 @@ wilcoxon.signed.rank.test <-
   result <- list(title = "Wilcoxon signed-rank test", varname = varname,
                  H0 = H0, alternative = alternative, cont.corr = cont.corr,
                  pval = pval, pval.stat = pval.stat, pval.note = pval.note,
-                 pval.approx = pval.approx, pval.approx.stat = pval.approx.stat,
-                 pval.approx.note = pval.approx.note, pval.exact = pval.exact,
+                 pval.asymp = pval.asymp, pval.asymp.stat = pval.asymp.stat,
+                 pval.asymp.note = pval.asymp.note, pval.exact = pval.exact,
                  pval.exact.stat = pval.exact.stat,
                  pval.exact.note = pval.exact.note, targetCIwidth = CI.width,
                  actualCIwidth = actualCIwidth, CI.lower = CI.lower,
