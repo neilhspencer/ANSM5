844f3b326d7ea4d7d50691356ec28ea6a570f703
Neil Spencer <neilhspencer@gmail.com>
2023-03-17 16:01:31 +0000

    Adding test for Example 5.1 and correcting other test

--- tests/testthat/test-shapiro.test.ANSM.R
+++ tests/testthat/test-shapiro.test.ANSM.R
@@ -1,5 +1,11 @@
 test_that("p-val works", {
   ages <- c(11, 13, 14, 22, 29, 30, 41, 41, 52, 55, 56, 59, 65, 65, 66, 74, 74,
             75, 77, 81, 82, 82, 82, 82, 83, 85, 85, 87, 87, 88)
-  expect_equal(shapiro.test.ANSM(ages)$pval, 0.1361)
+  expect_equal(shapiro.test.ANSM(ages)$pval.asymp, 0.1361)
 })
+
+test_that("Example 5.1", {
+  diffs <- c(-1, 2, 7, 14, 16, 16, 19, 20, 20, 22, 30, 33)
+  expect_equal(shapiro.test.ANSM(diffs)$pval.asymp, 0.790)
+})
+
