968f958a966ced2e3d81f4b04ff0eeb5544ef95f
Neil Spencer <neilhspencer@gmail.com>
2023-03-24 17:17:48 +0000

    Amend to allow test statistic to be string and change "toprint" to "to.print"

--- R/print.ANSMtest.R
+++ R/print.ANSMtest.R
@@ -1,25 +1,25 @@
-print.ANSMtest <- function(toprint) {
+print.ANSMtest <- function(to.print) {
   #print space
   cat("\n")
 
   #print test/variable name
-  cat(paste0(toprint$title, " for ", toprint$varname), "\n")
+  cat(paste0(to.print$title, " for ", to.print$varname), "\n")
 
   #print space
   cat("\n")
 
   #print hypotheses
-  if (!is.null(toprint$H0)){
-    if (is.character(toprint$H0)){
-      cat(toprint$H0)
+  if (!is.null(to.print$H0)){
+    if (is.character(to.print$H0)){
+      cat(to.print$H0)
     }else{
-      cat(paste0("Null hypothesis: theta = ", toprint$H0), "\n")
-      if (toprint$alternative == "two.sided") {
-        cat(paste0("Alternative hypothesis (2-sided): theta != ", toprint$H0), "\n")
-      }else if (toprint$alternative == "less") {
-        cat(paste0("Alternative hypothesis (1-sided): theta < ", toprint$H0), "\n")
+      cat(paste0("Null hypothesis: theta = ", to.print$H0), "\n")
+      if (to.print$alternative == "two.sided") {
+        cat(paste0("Alternative hypothesis (2-sided): theta != ", to.print$H0), "\n")
+      }else if (to.print$alternative == "less") {
+        cat(paste0("Alternative hypothesis (1-sided): theta < ", to.print$H0), "\n")
       }else{
-        cat(paste0("Alternative hypothesis (1-sided): theta > ", toprint$H0), "\n")
+        cat(paste0("Alternative hypothesis (1-sided): theta > ", to.print$H0), "\n")
       }
     }
     #print space
@@ -27,71 +27,83 @@ print.ANSMtest <- function(toprint) {
   }
 
   #print pval.exact
-  if (!is.null(toprint$pval.exact)){
-    if (!is.null(toprint$pval.exact.stat)){
-      cat(paste0("Statistic for exact test: ", round(toprint$pval.exact.stat,5)), "\n")
+  if (!is.null(to.print$pval.exact)){
+    if (!is.null(to.print$pval.exact.stat)){
+      if (is.character(to.print$pval.exact.stat)){
+        cat(paste0("Statistic for exact test: ", to.print$pval.exact.stat), "\n")
+      }else{
+        cat(paste0("Statistic for exact test: ", round(to.print$pval.exact.stat,5)), "\n")
+      }
     }
-    if (is.na(toprint$pval.exact)){
+    if (is.na(to.print$pval.exact)){
       cat("Exact p-value cannot be calculated\n")
-    }else if (toprint$pval.exact < 0.00001){
+    }else if (to.print$pval.exact < 0.00001){
       cat("Exact p-value: < 0.00001\n")
     }else{
-      cat(paste0("Exact p-value: ", sprintf("%.5f",toprint$pval.exact), "\n"))
+      cat(paste0("Exact p-value: ", sprintf("%.5f",to.print$pval.exact), "\n"))
     }
   }
-  if (!is.null(toprint$pval.exact.note)){cat(toprint$pval.exact.note, "\n")}
+  if (!is.null(to.print$pval.exact.note)){cat(to.print$pval.exact.note, "\n")}
 
   #print exact CI
-  if (!is.null(toprint$CI.exact.lower) && !is.null(toprint$CI.exact.upper)){
-    if (is.na(toprint$CI.exact.lower) | is.na(toprint$CI.exact.upper)){
-      cat(paste0("Exact ", 100 * round(toprint$targetCIwidth, 5),
+  if (!is.null(to.print$CI.exact.lower) && !is.null(to.print$CI.exact.upper)){
+    if (is.na(to.print$CI.exact.lower) | is.na(to.print$CI.exact.upper)){
+      cat(paste0("Exact ", 100 * round(to.print$targetCIwidth, 5),
                  "% Confidence Interval cannot be calculated"), "\n")
-    }else if (!is.null(toprint$actualCIwidth.exact)){
-      cat(paste0("Exact ", 100 * round(toprint$targetCIwidth, 5),
+    }else if (!is.null(to.print$actualCIwidth.exact)){
+      cat(paste0("Exact ", 100 * round(to.print$targetCIwidth, 5),
                  "% Confidence Interval (",
-                 100 * round(toprint$actualCIwidth.exact, 5), "% achieved)"), "\n")
-      cat(paste0("(", sprintf("%.5f", toprint$CI.exact.lower), ", ",
-                 sprintf("%.5f", toprint$CI.exact.upper), ")"), "\n")
+                 100 * round(to.print$actualCIwidth.exact, 5), "% achieved)"), "\n")
+      cat(paste0("(", sprintf("%.5f", to.print$CI.exact.lower), ", ",
+                 sprintf("%.5f", to.print$CI.exact.upper), ")"), "\n")
     }else{
-      cat(paste0("Exact ", 100 * round(toprint$targetCIwidth, 5),
+      cat(paste0("Exact ", 100 * round(to.print$targetCIwidth, 5),
                  "% Confidence Interval"), "\n")
-      cat(paste0("(", sprintf("%.5f", toprint$CI.exact.lower), ", ",
-                 sprintf("%.5f", toprint$CI.exact.upper), ")"), "\n")
+      cat(paste0("(", sprintf("%.5f", to.print$CI.exact.lower), ", ",
+                 sprintf("%.5f", to.print$CI.exact.upper), ")"), "\n")
     }
   }
-  if (!is.null(toprint$CI.exact.note)){cat(toprint$CI.exact.note, "\n")}
+  if (!is.null(to.print$CI.exact.note)){cat(to.print$CI.exact.note, "\n")}
 
   #print space
-  if (!is.null(toprint$pval.exact) | !is.null(toprint$pval.exact.note) |
-      !is.null(toprint$CI.exact.lower) | !is.null(toprint$CI.exact.note))
+  if (!is.null(to.print$pval.exact) | !is.null(to.print$pval.exact.note) |
+      !is.null(to.print$CI.exact.lower) | !is.null(to.print$CI.exact.note))
   {cat("\n")}
 
   #print pval
-  if (!is.null(toprint$pval)){
-    if (!is.null(toprint$pval.stat)){
-      cat(paste0("Statistic for test: ", round(toprint$pval.stat,5)), "\n")
+  if (!is.null(to.print$pval)){
+    if (!is.null(to.print$pval.stat)){
+      if (is.character(to.print$pval.stat)){
+        cat(paste0("Statistic for test: ", to.print$pval.stat), "\n")
+      }else{
+        cat(paste0("Statistic for test: ", round(to.print$pval.stat,5)), "\n")
+      }
     }
-    if (is.na(toprint$pval)){
+    if (is.na(to.print$pval)){
       cat("p-value cannot be calculated\n")
-    }else if (toprint$pval < 0.00001){
+    }else if (to.print$pval < 0.00001){
       cat("p-value: < 0.00001\n")
     }else{
-      cat(paste0("p-value: ", sprintf("%.5f",toprint$pval), "\n"))
+      cat(paste0("p-value: ", sprintf("%.5f",to.print$pval), "\n"))
     }
   }
-  if (!is.null(toprint$pval.note)){cat(toprint$pval.note, "\n")}
+  if (!is.null(to.print$pval.note)){cat(to.print$pval.note, "\n")}
 
   #print space
-  if (!is.null(toprint$pval) | !is.null(toprint$pval.note))
+  if (!is.null(to.print$pval) | !is.null(to.print$pval.note))
   {cat("\n")}
 
   #print pval.asymp
-  if (!is.null(toprint$pval.asymp)){
-    if (!is.null(toprint$pval.asymp.stat)){
-      cat(paste0("Statistic for asymptotic test: ", round(toprint$pval.asymp.stat,5)), "\n")
+  if (!is.null(to.print$pval.asymp)){
+    if (!is.null(to.print$pval.asymp.stat)){
+      if (is.character(to.print$pval.asymp.stat)){
+        cat(paste0("Statistic for asymptotic test: ", to.print$pval.asymp.stat), "\n")
+      }else{
+        cat(paste0("Statistic for asymptotic test: ", round(to.print$pval.asymp.stat,5)), "\n")
+      }
     }
-    if (!is.null(toprint$cont.corr)){
-      if (toprint$cont.corr == TRUE){
+    if (!is.null(to.print$cont.corr)){
+      if (to.print$cont.corr == TRUE){
         pval.asymp.label = "Asymptotic p-value (continuity correction used): "
       }else{
         pval.asymp.label = "Asymptotic p-value (continuity correction not used): "
@@ -99,77 +111,81 @@ print.ANSMtest <- function(toprint) {
     }else{
       pval.asymp.label = "Asymptotic p-value: "
     }
-    if (is.na(toprint$pval.asymp)){
+    if (is.na(to.print$pval.asymp)){
       cat("Asymptotic p-value cannot be calculated\n")
-    }else if (toprint$pval.asymp < 0.00001){
+    }else if (to.print$pval.asymp < 0.00001){
       cat(paste0(pval.asymp.label, "< 0.00001\n"))
     }else{
-      cat(paste0(pval.asymp.label, sprintf("%.5f",toprint$pval.asymp), "\n"))
+      cat(paste0(pval.asymp.label, sprintf("%.5f",to.print$pval.asymp), "\n"))
     }
   }
-  if (!is.null(toprint$pval.asymp.note)){cat(toprint$pval.asymp.note, "\n")}
+  if (!is.null(to.print$pval.asymp.note)){cat(to.print$pval.asymp.note, "\n")}
 
   #print asymp CI
-  if (!is.null(toprint$CI.asymp.lower) && !is.null(toprint$CI.asymp.upper)){
-    if (is.na(toprint$CI.asymp.lower) | is.na(toprint$CI.asymp.upper)){
-      cat(paste0("Asymptotic ", 100 * round(toprint$targetCIwidth, 5),
+  if (!is.null(to.print$CI.asymp.lower) && !is.null(to.print$CI.asymp.upper)){
+    if (is.na(to.print$CI.asymp.lower) | is.na(to.print$CI.asymp.upper)){
+      cat(paste0("Asymptotic ", 100 * round(to.print$targetCIwidth, 5),
                  "% Confidence Interval cannot be calculated"), "\n")
     }else{
-      cat(paste0("Asymptotic ", 100 * round(toprint$targetCIwidth, 5),
+      cat(paste0("Asymptotic ", 100 * round(to.print$targetCIwidth, 5),
                  "% Confidence Interval"), "\n")
-      cat(paste0("(", sprintf("%.5f", toprint$CI.asymp.lower), ", ",
-                 sprintf("%.5f", toprint$CI.asymp.upper), ")"), "\n")
+      cat(paste0("(", sprintf("%.5f", to.print$CI.asymp.lower), ", ",
+                 sprintf("%.5f", to.print$CI.asymp.upper), ")"), "\n")
     }
   }
-  if (!is.null(toprint$CI.asymp.note)){cat(toprint$CI.asymp.note, "\n")}
+  if (!is.null(to.print$CI.asymp.note)){cat(to.print$CI.asymp.note, "\n")}
 
   #print space
-  if (!is.null(toprint$pval.asymp) | !is.null(toprint$pval.asymp.note) |
-      !is.null(toprint$CI.asymp.lower) | !is.null(toprint$CI.asymp.note))
+  if (!is.null(to.print$pval.asymp) | !is.null(to.print$pval.asymp.note) |
+      !is.null(to.print$CI.asymp.lower) | !is.null(to.print$CI.asymp.note))
   {cat("\n")}
 
   #print pval.mc
-  if (!is.null(toprint$pval.mc)){
-    if (!is.null(toprint$pval.mc.stat)){
-      cat(paste0("Statistic for Monte Carlo test: ", round(toprint$pval.mc.stat,5)), "\n")
+  if (!is.null(to.print$pval.mc)){
+    if (!is.null(to.print$pval.mc.stat)){
+      if (is.character(to.print$pval.mc.stat)){
+        cat(paste0("Statistic for Monte Carlo test: ", to.print$pval.mc.stat), "\n")
+      }else{
+        cat(paste0("Statistic for Monte Carlo test: ", round(to.print$pval.mc.stat,5)), "\n")
+      }
     }
-    pval.mc.label = paste0("Monte Carlo p-value (", sprintf("%1$d",toprint$nsims.mc),
+    pval.mc.label = paste0("Monte Carlo p-value (", sprintf("%1$d",to.print$nsims.mc),
                            " simulations): ")
-    if (toprint$pval.mc < 0.00001){
+    if (to.print$pval.mc < 0.00001){
       cat(paste0(pval.mc.label, "< 0.00001\n"))
     }else{
-      if (toprint$nsims.mc >= 100000){
-        cat(paste0(pval.mc.label, sprintf("%.5f",toprint$pval.mc), "\n"))
-      }else if (toprint$nsims.mc >= 10000){
-        cat(paste0(pval.mc.label, sprintf("%.4f",toprint$pval.mc), "\n"))
+      if (to.print$nsims.mc >= 100000){
+        cat(paste0(pval.mc.label, sprintf("%.5f",to.print$pval.mc), "\n"))
+      }else if (to.print$nsims.mc >= 10000){
+        cat(paste0(pval.mc.label, sprintf("%.4f",to.print$pval.mc), "\n"))
       }else{
-        cat(paste0(pval.mc.label, sprintf("%.3f",toprint$pval.mc), "\n"))
+        cat(paste0(pval.mc.label, sprintf("%.3f",to.print$pval.mc), "\n"))
       }
     }
   }
-  if (!is.null(toprint$pval.mc.note)){cat(toprint$pval.mc.note, "\n")}
+  if (!is.null(to.print$pval.mc.note)){cat(to.print$pval.mc.note, "\n")}
 
   #print Monte Carlo CI
-  if (!is.null(toprint$CI.mc.lower) && !is.null(toprint$CI.mc.upper)){
-    if (is.na(toprint$CI.mc.lower) | is.na(toprint$CI.mc.upper)){
-      cat(paste0("Monte Carlo ", 100 * round(toprint$targetCIwidth, 5),
+  if (!is.null(to.print$CI.mc.lower) && !is.null(to.print$CI.mc.upper)){
+    if (is.na(to.print$CI.mc.lower) | is.na(to.print$CI.mc.upper)){
+      cat(paste0("Monte Carlo ", 100 * round(to.print$targetCIwidth, 5),
                  "% Confidence Interval cannot be calculated"), "\n")
     }else{
-      cat(paste0("Monte Carlo ", 100 * round(toprint$targetCIwidth, 5),
-                 "% Confidence Interval (", toprint$nsims.mc, " simulations)"), "\n")
-      cat(paste0("(", toprint$CI.mc.lower, ", ", toprint$CI.mc.upper, ")"), "\n")
+      cat(paste0("Monte Carlo ", 100 * round(to.print$targetCIwidth, 5),
+                 "% Confidence Interval (", to.print$nsims.mc, " simulations)"), "\n")
+      cat(paste0("(", to.print$CI.mc.lower, ", ", to.print$CI.mc.upper, ")"), "\n")
     }
   }
-  if (!is.null(toprint$CI.mc.note)){cat(toprint$CI.mc.note, "\n")}
+  if (!is.null(to.print$CI.mc.note)){cat(to.print$CI.mc.note, "\n")}
 
   #print space
-  if (!is.null(toprint$pval.mc) | !is.null(toprint$pval.mc.note) |
-      !is.null(toprint$CI.mc.lower) | !is.null(toprint$CI.mc.note))
+  if (!is.null(to.print$pval.mc) | !is.null(to.print$pval.mc.note) |
+      !is.null(to.print$CI.mc.lower) | !is.null(to.print$CI.mc.note))
   {cat("\n")}
 
   #print test note
-  if (!is.null(toprint$test.note)){
-    cat(toprint$test.note, "\n")
+  if (!is.null(to.print$test.note)){
+    cat(to.print$test.note, "\n")
     cat("\n")
   }
 }
