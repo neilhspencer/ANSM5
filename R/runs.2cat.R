f08231eebcdb2e37f389be3112dc78ad825d43fe
Neil Spencer <neilhspencer@gmail.com>
2023-04-14 18:03:45 +0100

    Correcting bug and updating for addition of second variable to system

--- R/runs.2cat.R
+++ R/runs.2cat.R
@@ -8,9 +8,10 @@ runs.2cat <-
     alternative <- match.arg(alternative)
 
     #labels
-    varname <- deparse(substitute(x))
+    varname1 <- deparse(substitute(x))
 
     #default outputs
+    varname2 <- NULL
     CI.width <- NULL
     pval <- NULL
     pval.stat <- NULL
@@ -100,7 +101,7 @@ runs.2cat <-
             break
           }
         }
-        if (is.null(pval.exact)){cumup <- cumup1 + cumup2} #will sum to 1
+        if (is.null(pval.exact)){pval.exact <- cumup1 + cumup2} #will sum to 1
       }else if (alternative == "less"){
         pval.exact <- 0
         for (i in 1:nruns){
@@ -177,7 +178,8 @@ runs.2cat <-
     }
 
     #return
-    result <- list(title = "Runs test for two categories" , varname = varname,
+    result <- list(title = "Runs test for two categories" , varname1 = varname1,
+                   varname2 = varname2,
                    H0 = H0, alternative = alternative, cont.corr = cont.corr,
                    pval = pval, pval.stat = pval.stat, pval.note = pval.note,
                    pval.exact = pval.exact, pval.exact.stat = pval.exact.stat,
