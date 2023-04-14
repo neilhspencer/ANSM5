de26ce5ae57f72c2d847d9c0e21cbd42b4905aea
Neil Spencer <neilhspencer@gmail.com>
2023-04-14 17:59:58 +0100

    New function and tests

---
+++ R/wilcoxon.mann.whitney.R
@@ -0,0 +1,251 @@
+#' @importFrom stats complete.cases wilcox.test
+#' @importFrom utils combn
+wilcoxon.mann.whitney <-
+  function(x, y, H0 = NULL, alternative=c("two.sided", "less", "greater"),
+           cont.corr = TRUE, CI.width = 0.95, max.exact.cases = 1000,
+           do.asymp = FALSE, do.exact = TRUE, do.CI = TRUE) {
+    stopifnot(is.vector(x), is.numeric(x), is.vector(y), is.numeric(y),
+              ((is.numeric(H0) && length(H0) == 1) | is.null(H0)),
+              is.numeric(max.exact.cases), length(max.exact.cases) == 1,
+              is.logical(cont.corr) == TRUE, CI.width > 0, CI.width < 1,
+              is.logical(do.asymp) == TRUE, is.logical(do.exact) == TRUE,
+              is.logical(do.CI) == TRUE)
+    alternative <- match.arg(alternative)
+
+    #labels
+    varname1 <- deparse(substitute(x))
+    varname2 <- deparse(substitute(y))
+
+    #default outputs
+    pval <- NULL
+    pval.stat <- NULL
+    pval.note <- NULL
+    pval.asymp <- NULL
+    pval.asymp.stat <- NULL
+    pval.asymp.note <- NULL
+    pval.exact <- NULL
+    pval.exact.stat <- NULL
+    pval.exact.note <- NULL
+    pval.mc <- NULL
+    pval.mc.stat <- NULL
+    nsims.mc <- NULL
+    pval.mc.note <- NULL
+    actualCIwidth.exact <- NULL
+    CI.exact.lower <- NULL
+    CI.exact.upper <- NULL
+    CI.exact.note <- NULL
+    CI.asymp.lower <- NULL
+    CI.asymp.upper <- NULL
+    CI.asymp.note <- NULL
+    CI.mc.lower <- NULL
+    CI.mc.upper <- NULL
+    CI.mc.note <- NULL
+    test.note <- NULL
+
+    #prepare
+    x <- x[complete.cases(x)] #remove missing cases
+    y <- y[complete.cases(y)] #remove missing cases
+    if (!is.null(H0)) {
+      xy <- c(x - H0, y)
+      varname1 <- paste0(varname1, " - ", H0)
+    }else{
+      H0 <- 0
+      xy <- c(x, y)
+    }
+    s <- rank(abs(xy), ties.method = "average")
+    ranksumx <- sum(s[1 : length(x)])
+    ranksumy <- sum(s) - ranksumx
+    mannwhitneyux <- ranksumx - length(x) * (length(x) + 1) / 2
+    mannwhitneyuy <- ranksumy - length(y) * (length(y) + 1) / 2
+    stat <- paste0("\n", ranksumx, " (rank sum from ", varname1, "), ",
+                   ranksumy, " (rank sum from ", varname2, ")\n", mannwhitneyux,
+                   " (Mann-Whitney U from ", varname1, "), ", mannwhitneyuy,
+                   " (Mann-Whitney U from ", varname2, ")")
+
+    #give asymptotic output if exact not possible
+    if (do.exact && n > max.exact.cases){
+      do.asymp <- TRUE
+    }
+
+    #check for ties
+    tiesexist = !all(s == round(s,0)) # TRUE if ties exist
+
+    #exact p-value & CI
+    if (do.exact && tiesexist){
+      OverflowState <- FALSE
+      try_result <- suppressWarnings(try(
+        combins <- combn(length(xy), min(length(x), length(y))), silent = TRUE)
+        )
+      if (class(try_result) == "try-error"){
+        OverflowState <- TRUE
+      }
+        if (OverflowState){
+        do.asymp <- TRUE
+      }
+    }
+    if (do.exact && ((!tiesexist && n <= max.exact.cases) |
+                     (tiesexist && !OverflowState))){
+      pval.exact.stat <- stat
+      if (!tiesexist){
+        wilcox.test.output <- wilcox.test(x, y, alternative = alternative,
+                                          mu = H0, exact = TRUE,
+                                          conf.int = do.CI,
+                                          conf.level = CI.width)
+        pval.exact <- wilcox.test.output$p.value
+        if (do.CI){
+          CI.exact.lower <- wilcox.test.output$conf.int[1]
+          CI.exact.upper <- wilcox.test.output$conf.int[2]
+        }
+      }else{
+        nfrom <- min(length(x), length(y))
+        permfrom <- s * 2
+        permfrom <- sort(permfrom)
+        permsums <- rep(0,sum(tail(permfrom, nfrom)))
+        for (i in 1:dim(combins)[2]){
+          tmpsum <- sum(permfrom[combins[,i]])
+          permsums[tmpsum] <- permsums[tmpsum] + 1
+        }
+        ranksum <- ifelse(length(x) < length(y), ranksumx * 2, ranksumy * 2)
+        if (length(x) < length(y) && alternative == "less"){
+          pval.exact.less <-
+            sum(permsums[1:ranksum]) / sum(permsums)
+          pval.exact.greater <-
+            sum(permsums[ranksum:length(permsums)]) / sum(permsums)
+        }else if (length(x) < length(y) && alternative == "greater"){
+          pval.exact.less <-
+            sum(permsums[ranksum:length(permsums)]) / sum(permsums)
+          pval.exact.greater <-
+            sum(permsums[1:ranksum]) / sum(permsums)
+        }else if (length(x) >= length(y) && alternative == "less"){
+          pval.exact.less <-
+            sum(permsums[ranksum:length(permsums)]) / sum(permsums)
+          pval.exact.greater <-
+            sum(permsums[1:ranksum]) / sum(permsums)
+        }else if (length(x) >= length(y) && alternative == "greater"){
+          pval.exact.less <-
+            sum(permsums[1:ranksum]) / sum(permsums)
+          pval.exact.greater <-
+            sum(permsums[ranksum:length(permsums)]) / sum(permsums)
+        }else{
+          pval.exact.less <-
+            sum(permsums[1:ranksum]) / sum(permsums)
+          pval.exact.greater <-
+            sum(permsums[ranksum:length(permsums)]) / sum(permsums)
+        }
+        if (alternative=="two.sided"){
+          pval.exact <- min(pval.exact.less, pval.exact.greater) * 2
+        }else if (alternative == "less"){
+          pval.exact <- pval.exact.less
+        }else if (alternative == "greater"){
+          pval.exact <- pval.exact.greater
+        }
+        if (do.CI){
+          CI.exact.lower <- NULL
+          CI.exact.upper <- NULL
+        }
+      }
+    }
+
+    #asymptotic p-value and CI (with/without continuity correction)
+    if (do.asymp){
+      wilcox.test.output <- wilcox.test(x, y, alternative = alternative,
+                                        mu = H0, exact = FALSE,
+                                        correct = cont.corr, conf.int = do.CI,
+                                        conf.level = CI.width)
+      pval.asymp.stat <- stat
+      pval.asymp <- wilcox.test.output$p.value
+      if (do.CI){
+        CI.asymp.lower <- wilcox.test.output$conf.int[1]
+        CI.asymp.upper <- wilcox.test.output$conf.int[2]
+      }
+    }
+
+    #check if message needed
+    if (!do.asymp && !do.exact) {
+      test.note <- paste("Neither exact nor asymptotic test/confidence interval ",
+                         "requested")
+    }else if (n > max.exact.cases) {
+      affected <- NULL
+      if (do.exact && do.CI && !is.null(H0)){
+        affected <- "exact test and confidence interval"
+      }else if (do.exact && do.CI && is.null(H0)) {
+        affected <- "exact confidence interval"
+      }else if (do.exact) {
+        affected <- "exact test"
+      }
+      if (!is.null(affected)){
+        test.note <- paste0("NOTE: Number of useful cases greater than current ",
+                            "maximum allowed for exact\ncalculations required ",
+                            "for ", affected, " (max.exact.cases = ",
+                            sprintf("%1.0f", max.exact.cases), ")")
+      }
+    }
+    if (tiesexist && OverflowState){
+      if (!is.null(test.note)){
+        test.note <- paste0(test.note, "\n")
+      }
+      test.note <- paste0(test.note, "NOTE: Ties exist in data and sample ",
+                          "too large for exact\ncalculations required ",
+                          "for exact test")
+    }
+    if (tiesexist && do.exact && do.CI){
+      if (!is.null(test.note)){
+        test.note <- paste0(test.note, "\n")
+      }
+      if (do.CI){
+        test.note <- paste0(test.note, "NOTE: Ties exist in data so exact ",
+                            "confidence interval not available")
+      }else if (!do.CI){
+        test.note <- paste0(test.note, "NOTE: Ties exist in data so mid-ranks ",
+                            "used for asymptotic test")
+      }
+    }
+    if (tiesexist && do.asymp){
+      if (!is.null(test.note)){
+        test.note <- paste0(test.note, "\n")
+      }
+      if (do.CI){
+        test.note <- paste0(test.note, "NOTE: Ties exist in data so mid-ranks ",
+                            "used for asymptotic test and confidence interval")
+      }else if (!do.CI){
+        test.note <- paste0(test.note, "NOTE: Ties exist in data so mid-ranks ",
+                            "used for asymptotic test")
+      }
+    }
+
+    #define hypotheses
+    if (alternative == "two.sided"){
+      H0 <- paste0("H0: samples are from the same population\n",
+                   "H1: samples differ in location\n")
+    }else if (alternative == "less"){
+      H0 <- paste0("H0: samples are from the same population\n",
+                   "H1: location of ", varname1, " is less than location of ",
+                   varname2, "\n")
+    }else if (alternative == "greater"){
+      H0 <- paste0("H0: samples are from the same population\n",
+                   "H1: location of ", varname1, " is greater than location of ",
+                   varname2, "\n")
+    }
+
+    #return
+    result <- list(title = "Wilcoxon-Mann-Whitney test", varname1 = varname1,
+                   varname2 = varname2, H0 = H0,
+                   alternative = alternative, cont.corr = cont.corr, pval = pval,
+                   pval.stat = pval.stat, pval.note = pval.note,
+                   pval.exact = pval.exact, pval.exact.stat = pval.exact.stat,
+                   pval.exact.note = pval.exact.note, targetCIwidth = CI.width,
+                   actualCIwidth.exact = actualCIwidth.exact,
+                   CI.exact.lower = CI.exact.lower,
+                   CI.exact.upper = CI.exact.upper, CI.exact.note = CI.exact.note,
+                   pval.asymp = pval.asymp, pval.asymp.stat = pval.asymp.stat,
+                   pval.asymp.note = pval.asymp.note,
+                   CI.asymp.lower = CI.asymp.lower,
+                   CI.asymp.upper = CI.asymp.upper, CI.asymp.note = CI.asymp.note,
+                   pval.mc = pval.mc, pval.mc.stat = pval.mc.stat,
+                   nsims.mc = nsims.mc, pval.mc.note = pval.mc.note,
+                   CI.mc.lower = CI.mc.lower, CI.mc.upper = CI.mc.upper,
+                   CI.mc.note = CI.mc.note,
+                   test.note = test.note)
+    class(result) <- "ANSMtest"
+    return(result)
+  }
---
+++ tests/testthat/test-wilcoxon.mann.whitney.R
@@ -0,0 +1,54 @@
+test_that("Example 6.1", {
+  GroupA <- c(23, 18, 17, 25, 22, 19,  31, 26, 29, 33)
+  GroupB <- c(21, 28, 32, 30, 41, 24,  35, 34, 27, 39, 36)
+  expect_equal(wilcoxon.mann.whitney(GroupA, GroupB)$pval.exact.stat,
+               paste0("\n","76 (rank sum from GroupA), ",
+                      "155 (rank sum from GroupB)", "\n",
+                      "21 (Mann-Whitney U from GroupA), ",
+                      "89 (Mann-Whitney U from GroupB)"))
+  expect_equal(wilcoxon.mann.whitney(GroupA, GroupB)$pval.exact, 0.015871126)
+})
+
+test_that("Example 6.2", {
+  GroupA <- c(23, 18, 17, 25, 22, 19,  31, 26, 29, 33)
+  GroupB <- c(21, 28, 32, 30, 41, 24,  35, 34, 27, 39, 36)
+  expect_equal(wilcoxon.mann.whitney(GroupA, GroupB)$pval.exact.stat,
+               paste0("\n","76 (rank sum from GroupA), ",
+                      "155 (rank sum from GroupB)", "\n",
+                      "21 (Mann-Whitney U from GroupA), ",
+                      "89 (Mann-Whitney U from GroupB)"))
+  expect_equal(wilcoxon.mann.whitney(GroupA, GroupB)$pval.exact, 0.015871126)
+})
+
+test_that("Example 6.3", {
+  GroupA <- c(23, 18, 17, 25, 22, 19,  31, 26, 29, 33)
+  GroupB <- c(21, 28, 32, 30, 41, 24,  35, 34, 27, 39, 36)
+  expect_equal(wilcoxon.mann.whitney(GroupA, GroupB)$CI.exact.lower, -13)
+  expect_equal(wilcoxon.mann.whitney(GroupA, GroupB)$CI.exact.upper, -2)
+  expect_equal(wilcoxon.mann.whitney(GroupA, GroupB,
+                                     CI.width = 0.99)$CI.exact.lower, -16)
+  expect_equal(wilcoxon.mann.whitney(GroupA, GroupB,
+                                     CI.width = 0.99)$CI.exact.upper, 1)
+})
+
+test_that("Example 6.4", {
+  GroupA <- c(16, 18, 19, 22, 22, 25, 28, 28, 28, 31, 33)
+  GroupB <- c(22, 23, 25, 27, 27, 28, 30, 32, 33, 35, 36, 38, 38)
+  expect_equal(wilcoxon.mann.whitney(GroupA, GroupB)$pval.exact, 0.03586652)
+})
+
+test_that("Example 6.5", {
+  GroupA <- c(1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3)
+  GroupB <- c(2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3)
+  expect_equal(wilcoxon.mann.whitney(GroupA, GroupB)$pval.exact, 0.044156106)
+})
+
+test_that("Example 6.6", {
+  McGamma <- c(13, 13, 22, 26, 33, 33, 59, 72, 72, 72, 77, 78, 78, 80, 81, 82,
+               85, 85, 85, 86, 88)
+  McBeta <- c(0, 19, 22, 30, 31, 37, 55, 56, 66, 66, 67, 67, 68, 71, 73, 75, 75,
+              78, 79, 82, 83, 83, 88, 96)
+  expect_equal(wilcoxon.mann.whitney(McGamma, McBeta, do.exact = FALSE,
+                                     do.asymp = TRUE,
+                                     cont.corr = FALSE)$pval.asymp, 0.41896844)
+})
