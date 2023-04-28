bf10d05fdfb42687f32a1b1087edb4197ffb5c26
Neil Spencer <neilhspencer@gmail.com>
2023-04-28 17:47:32 +0100

    Creation of data for chapter 3 in package

---
+++ R/data.R
@@ -0,0 +1,36 @@
+#' Data used in Chapter 3
+#'
+#' @description
+#' Data used in Chapter 3 of "Applied Nonparametric Statistical Methods" (5th edition)
+#'
+#' * sampleI (Examples 3.1, 3.2, 3.3, Exercise 3.17)
+#' * sampleII (Examples 3.1, 3.2, 3.3, Exercise 3.17)
+#' * heartrates1 (Examples 3.4, 3.11)
+#' * heartrates2 (Examples 3.5, 3.6, 3.7)
+#' * withties (Example 3.8)
+#' * tiesifrounded1 (Example 3.8)
+#' * tiesifrounded2 (Example 3.8)
+#' * ages (Example 3.9)
+#' * sampleA (Examples 3.12, 3.13)
+#' * sampleB (Examples 3.12, 3.13)
+#' * sampleC (amended version of sampleA, used in example 3.12)
+#' * sampleD (amended version of sampleA, used in example 3.12)
+#' * heartrates2a (amended version of heartrates2, used in example 3.14)
+#' * heartrates2b (amended version of heartrates2, used in example 3.14)
+#' * sampleIa (amended version of sampleI, used in exercise 3.1)
+#' * parkingtime (Exercise 3.3)
+#' * Sprobs (Exercise 3.4)
+#' * children (Exercise 3.6)
+#' * fishlengths (Exercises 3.7, 3.11)
+#' * weightloss (Exercise 3.12)
+#' * sleeptime (Exercise 3.10)
+#' * plants (Exercise 3.13)
+#' * birthprops (Exercise 3.14)
+#' * assembly (Exercise 3.15)
+#' * weightchange (Exercise 3.16)
+#'
+#'
+#' @format ## `ch3data`
+#' A list with 25 data objects
+#' @source "Applied Nonparametric Statistical Methods" (5th edition)
+"ch3data"
