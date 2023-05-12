459d8519ae229ec202a43b6f9acda1e3e3895d6f
Neil Spencer <neilhspencer@gmail.com>
2023-05-12 17:23:37 +0100

    Adding test

--- tests/testthat/test-median.test.R
+++ tests/testthat/test-median.test.R
@@ -4,3 +4,12 @@ test_that("Example 6.7", {
                25, 25, 27, 32)
   expect_equal(median.test(Males, Females)$pval.exact, 0.38454106)
 })
+
+test_that("Example 6.8", {
+  Males <- c(15, 16, 20, 24, 27, 31, 34)
+  Females <- c(8, 9, 11, 11, 14, 15, 16, 16, 16, 17, 18, 18, 19, 21, 23, 23, 25,
+               25, 25, 27, 32)
+  expect_equal(median.test(Males, Females, CI.width = 0.90)$CI.exact.lower, -15)
+  expect_equal(median.test(Males, Females, CI.width = 0.90)$CI.exact.upper, 3)
+  expect_equal(median.test(Males, Females, CI.width = 0.90)$actualCIwidth, 0.9231884)
+})
