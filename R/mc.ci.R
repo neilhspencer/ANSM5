4a7d4f9bc969705f3f6580b3e0deeb0abc09205f
Neil Spencer <neilhspencer@gmail.com>
2023-05-15 15:01:58 +0100

    Update to include CI for difference in medians

--- R/mc.ci.R
+++ R/mc.ci.R
@@ -1,22 +1,37 @@
 #' @importFrom stats median quantile
 mc.ci <-
-  function(x, CI.width = 0.95, nsims.mc = 10000, seed = NULL) {
-    stopifnot(is.vector(x), is.numeric(x), is.numeric(CI.width),
+  function(x, y = NULL, CI.width = 0.95, nsims.mc = 10000, seed = NULL) {
+    stopifnot(is.vector(x), is.numeric(x), is.vector(y) | is.null(y),
+              is.numeric(y) | is.null(y), is.numeric(CI.width),
               length(CI.width) == 1, CI.width > 0, CI.width < 1,
               is.numeric(nsims.mc), length(nsims.mc) == 1,
               is.numeric(seed) | is.null(seed),
               length(seed) == 1 | is.null(seed))
 
     #Monte Carlo confidence interval
-    median.vec <- rep(NA, nsims.mc)
-    n <- length(x)
-    if (!is.null(seed)){set.seed(seed)}
-    for (i in 1:nsims.mc){
-      x.2 <- sample(x, n, replace = TRUE)
-      median.vec[i] <- median(x.2)
+    if (is.null(y)){ #for one sample
+      median.vec <- rep(NA, nsims.mc)
+      n <- length(x)
+      if (!is.null(seed)){set.seed(seed)}
+      for (i in 1:nsims.mc){
+        x.2 <- sample(x, n, replace = TRUE)
+        median.vec[i] <- median(x.2)
+      }
+      mc.ci.lower <- quantile(median.vec, (1 - CI.width) / 2)
+      mc.ci.upper <- quantile(median.vec, 1 - (1 - CI.width) / 2)
+    }else{ #for two samples
+      mediandiff.vec <- rep(NA, nsims.mc)
+      nx <- length(x)
+      ny <- length(y)
+      if (!is.null(seed)){set.seed(seed)}
+      for (i in 1:nsims.mc){
+        x.2 <- sample(x, nx, replace = TRUE)
+        y.2 <- sample(y, ny, replace = TRUE)
+        mediandiff.vec[i] <- median(x.2) - median(y.2)
+      }
+      mc.ci.lower <- quantile(mediandiff.vec, (1 - CI.width) / 2)
+      mc.ci.upper <- quantile(mediandiff.vec, 1 - (1 - CI.width) / 2)
     }
-    mc.ci.lower <- quantile(median.vec, (1 - CI.width) / 2)
-    mc.ci.upper <- quantile(median.vec, 1 - (1 - CI.width) / 2)
 
     #return
     return(c(as.numeric(mc.ci.lower), as.numeric(mc.ci.upper)))
