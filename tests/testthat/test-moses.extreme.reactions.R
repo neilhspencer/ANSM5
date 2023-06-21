adc2f0fbde7514b9e9ac0cbb38f645a1c581dcd3
Neil Spencer <neilhspencer@gmail.com>
2023-06-21 14:12:53 +0100

    Tidying code

--- tests/testthat/test-moses.extreme.reactions.R
+++ tests/testthat/test-moses.extreme.reactions.R
@@ -6,5 +6,4 @@ test_that("Example 6.14", {
   GroupA2 <- c(8, 9, 10, 12, 13, 15, 18, 20, 22)
   expect_equal(moses.extreme.reactions(GroupA2, GroupB)$pval.exact.stat, 13)
   expect_equal(moses.extreme.reactions(GroupA2, GroupB)$pval.exact, 0.03989045)
-
 })
