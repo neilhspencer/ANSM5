cc7188036c432335cb9be94118294513ce5a12f7
Neil Spencer <neilhspencer@gmail.com>
2023-10-13 16:45:47 +0100

    New test for function

--- tests/testthat/test-pearson.R
+++ tests/testthat/test-pearson.R
@@ -10,3 +10,15 @@ test_that("Section 10.1.2", {
   expect_equal(pearson(Q1, Q2, alternative = "greater", seed = 1)$pval.mc,
                0.11676)
 })
+
+test_that("Example 11.2", {
+  parentlimit <- c(0, 1, 2, 3, 4, 5, 6)
+  reportedtime <- c(2.5, 3.1, 3.4, 4, 4.6, 5.1, 11.1)
+  H0 <- 1
+  expect_equal(pearson(parentlimit, reportedtime - H0 * parentlimit,
+                       alternative = "two.sided")$stat, 0.139175554)
+  expect_equal(pearson(parentlimit, reportedtime - H0 * parentlimit,
+                       alternative = "two.sided")$pval.exact, 0.79246032)
+})
+
+
