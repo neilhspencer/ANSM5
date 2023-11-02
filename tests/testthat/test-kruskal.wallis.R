8cc2f7953cf4bca5c8b70038afde805b4c3ccdd4
Neil Spencer <neilhspencer@gmail.com>
2023-11-02 09:40:42 +0000

    Additional test

--- tests/testthat/test-kruskal.wallis.R
+++ tests/testthat/test-kruskal.wallis.R
@@ -22,3 +22,20 @@ test_that("Example 7.2", {
   expect_equal(kruskal.wallis(age, positions, do.exact = FALSE,
                               do.asymp = TRUE)$pval.asymp, 0.25485424)
 })
+
+test_that("Example 7.2", {
+  time <- factor(c(
+    rep("<100", 2), rep("100-199", 6), rep("200-299", 9), rep("300+", 0),
+    rep("<100", 0), rep("100-199", 1), rep("200-299", 7), rep("300+", 0),
+    rep("<100", 4), rep("100-199", 10), rep("200-299", 6), rep("300+", 0),
+    rep("<100", 1), rep("100-199", 6), rep("200-299", 5), rep("300+", 0),
+    rep("<100", 0), rep("100-199", 8), rep("200-299", 10), rep("300+", 2)),
+    levels = c("<100", "100-199", "200-299", "300+"))
+  cause <- factor(c(rep("A", 17), rep("B", 8), rep("C", 20), rep("D", 12),
+                  rep("E", 20)), levels = c("A", "B", "C", "D", "E"))
+  kw.out <- kruskal.wallis(as.numeric(time), cause, do.asymp = TRUE, nsims.mc = 100000, seed = 1)
+  expect_equal(kw.out$pval.mc.stat, 10.444765)
+  expect_equal(kw.out$pval.mc, 0.02859)
+  expect_equal(kw.out$pval.asymp.stat, 10.444765)
+  expect_equal(kw.out$pval.asymp, 0.033566401)
+})
