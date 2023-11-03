62f160355832b886de62cc772039db5536c30386
Neil Spencer <neilhspencer@gmail.com>
2023-11-03 12:39:47 +0000

    New tests

--- tests/testthat/test-chisq.test.ANSM.R
+++ tests/testthat/test-chisq.test.ANSM.R
@@ -83,3 +83,23 @@ test_that("Example 12.11", {
                                do.exact = FALSE,
                                do.asymp = TRUE)$pval.asymp, 0)
 })
+
+test_that("Example 13.1", {
+  physical.activity <- factor(c(rep("Daily", 167 + 320 + 123 + 157),
+                                rep("Less than daily", 388 + 937 + 604 + 1114)),
+                              levels = c("Daily", "Less than daily"))
+  tv.viewing <- factor(c(rep("2 hours or less", 167), rep("More than 2 hours", 320),
+                         rep("2 hours or less", 123), rep("More than 2 hours", 157),
+                         rep("2 hours or less", 388), rep("More than 2 hours", 937),
+                         rep("2 hours or less", 604), rep("More than 2 hours", 1114)),
+                       levels = c("2 hours or less", "More than 2 hours"))
+  gender <- factor(c(rep("Boy", 167 + 320), rep("Girl", 123 + 157),
+                     rep("Boy", 388 + 937), rep("Girl", 604 + 1114)),
+                   levels = c("Boy", "Girl"))
+  expect_equal(chisq.test.ANSM(physical.activity[gender == "Boy"],
+                                tv.viewing[gender == "Boy"], seed = 1)$pval.mc,
+               0.044709553)
+  expect_equal(chisq.test.ANSM(physical.activity[gender == "Girl"],
+                               tv.viewing[gender == "Girl"], seed = 1)$pval.mc,
+               0.0049899501)
+})
