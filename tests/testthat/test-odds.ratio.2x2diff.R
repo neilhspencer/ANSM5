01b12cd90b75aca20113667b3261a0036b9ec371
Neil Spencer <neilhspencer@gmail.com>
2023-11-12 00:02:37 +0000

    Testing function

--- tests/testthat/test-odds.ratio.2x2diff.R
+++ tests/testthat/test-odds.ratio.2x2diff.R
@@ -11,23 +11,35 @@ test_that("Example 13.2", {
                      rep("Boy", 388 + 937), rep("Girl", 604 + 1114)),
                    levels = c("Boy", "Girl"))
   expect_equal(odds.ratio.2x2diff(physical.activity, tv.viewing, gender,
-                                  do.mc = FALSE,
+                                  do.exact = FALSE,
                                   do.asymp = TRUE)$pval.asymp.stat, -0.7918946)
   expect_equal(odds.ratio.2x2diff(physical.activity, tv.viewing, gender,
-                                  do.mc = FALSE,
+                                  do.exact = FALSE,
                                   do.asymp = TRUE)$pval.asymp, 0.42842214)
   expect_equal(odds.ratio.2x2diff(physical.activity, tv.viewing, gender,
-                                  do.mc = FALSE,
+                                  do.exact = FALSE,
                                   do.asymp = TRUE)$CI.asymp.lower, -0.475127192)
   expect_equal(odds.ratio.2x2diff(physical.activity, tv.viewing, gender,
-                                  do.mc = FALSE,
+                                  do.exact = FALSE,
                                   do.asymp = TRUE)$CI.asymp.upper, 0.201675165)
+  expect_equal(odds.ratio.2x2diff(physical.activity, tv.viewing,
+                                  gender)$pval.exact.stat, -0.136726013)
+  expect_equal(odds.ratio.2x2diff(physical.activity, tv.viewing,
+                                  gender)$pval.exact, 0.44031545)
+  expect_equal(odds.ratio.2x2diff(physical.activity, tv.viewing,
+                                  gender)$CI.exact.lower, -0.34589494)
+  expect_equal(odds.ratio.2x2diff(physical.activity, tv.viewing,
+                                  gender)$CI.exact.upper, 0.34815705)
   expect_equal(odds.ratio.2x2diff(physical.activity, tv.viewing, gender,
-                                  seed = 1)$pval.mc.stat, -0.7918946)
+                                  do.exact = FALSE, do.mc = TRUE,
+                                  seed = 1)$pval.mc.stat, -0.136726013)
   expect_equal(odds.ratio.2x2diff(physical.activity, tv.viewing, gender,
-                                  seed = 1)$pval.mc, 0.43210)
+                                  do.exact = FALSE, do.mc = TRUE,
+                                  seed = 1)$pval.mc, 0.44313)
   expect_equal(odds.ratio.2x2diff(physical.activity, tv.viewing, gender,
+                                  do.exact = FALSE, do.mc = TRUE,
                                   seed = 1)$CI.mc.lower, -0.34449187)
   expect_equal(odds.ratio.2x2diff(physical.activity, tv.viewing, gender,
+                                  do.exact = FALSE, do.mc = TRUE,
                                   seed = 1)$CI.mc.upper, 0.34938077)
 })
