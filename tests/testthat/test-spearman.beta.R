a4fcc59c52af59f4c150215ea5e2aa2863158db0
Neil Spencer <neilhspencer@gmail.com>
2024-01-17 10:25:13 +0000

    Amending sort of results obtained in test

--- tests/testthat/test-spearman.beta.R
+++ tests/testthat/test-spearman.beta.R
@@ -4,7 +4,7 @@ test_that("Example 11.3", {
   expect_equal(spearman.beta(ch11$reportedtime, ch11$parentlimit,
                              H0 = 1)$pval.exact, 0.59484127)
   expect_equal(spearman.beta(ch11$reportedtime, ch11$parentlimit, H0 = 1,
-                             do.CI = TRUE)$CI.exact.lower, 0.5)
+                             do.CI = TRUE)$CI.sample.lower, 0.5)
   expect_equal(spearman.beta(ch11$reportedtime, ch11$parentlimit, H0 = 1,
-                             do.CI = TRUE)$CI.exact.upper, 1.925)
+                             do.CI = TRUE)$CI.sample.upper, 1.925)
 })
