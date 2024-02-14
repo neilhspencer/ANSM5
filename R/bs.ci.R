c7546fec52a385cbcd19502f844285b7fc1dad6d
Neil Spencer <neilhspencer@gmail.com>
2024-02-14 09:55:20 +0000

    Renaming mc.ci.R to bs.ci.R

--- R/bs.ci.R
+++ R/bs.ci.R
@@ -1,38 +1,38 @@
 #' @importFrom stats median quantile
-mc.ci <-
-  function(x, y = NULL, CI.width = 0.95, nsims.mc = 10000, seed = NULL) {
+bs.ci <-
+  function(x, y = NULL, CI.width = 0.95, nsims.bs = 10000, seed = NULL) {
     stopifnot(is.vector(x), is.numeric(x), is.vector(y) | is.null(y),
               is.numeric(y) | is.null(y), is.numeric(CI.width),
               length(CI.width) == 1, CI.width > 0, CI.width < 1,
-              is.numeric(nsims.mc), length(nsims.mc) == 1,
+              is.numeric(nsims.bs), length(nsims.bs) == 1,
               is.numeric(seed) | is.null(seed),
               length(seed) == 1 | is.null(seed))
 
     #Monte Carlo confidence interval
     if (is.null(y)){ #for one sample
-      median.vec <- rep(NA, nsims.mc)
+      median.vec <- rep(NA, nsims.bs)
       n <- length(x)
       if (!is.null(seed)){set.seed(seed)}
-      for (i in 1:nsims.mc){
+      for (i in 1:nsims.bs){
         x.2 <- sample(x, n, replace = TRUE)
         median.vec[i] <- median(x.2)
       }
-      mc.ci.lower <- quantile(median.vec, (1 - CI.width) / 2)
-      mc.ci.upper <- quantile(median.vec, 1 - (1 - CI.width) / 2)
+      bs.ci.lower <- quantile(median.vec, (1 - CI.width) / 2)
+      bs.ci.upper <- quantile(median.vec, 1 - (1 - CI.width) / 2)
     }else{ #for two samples
-      mediandiff.vec <- rep(NA, nsims.mc)
+      mediandiff.vec <- rep(NA, nsims.bs)
       nx <- length(x)
       ny <- length(y)
       if (!is.null(seed)){set.seed(seed)}
-      for (i in 1:nsims.mc){
+      for (i in 1:nsims.bs){
         x.2 <- sample(x, nx, replace = TRUE)
         y.2 <- sample(y, ny, replace = TRUE)
         mediandiff.vec[i] <- median(x.2) - median(y.2)
       }
-      mc.ci.lower <- quantile(mediandiff.vec, (1 - CI.width) / 2)
-      mc.ci.upper <- quantile(mediandiff.vec, 1 - (1 - CI.width) / 2)
+      bs.ci.lower <- quantile(mediandiff.vec, (1 - CI.width) / 2)
+      bs.ci.upper <- quantile(mediandiff.vec, 1 - (1 - CI.width) / 2)
     }
 
     #return
-    return(c(as.numeric(mc.ci.lower), as.numeric(mc.ci.upper)))
+    return(c(as.numeric(bs.ci.lower), as.numeric(bs.ci.upper)))
   }
