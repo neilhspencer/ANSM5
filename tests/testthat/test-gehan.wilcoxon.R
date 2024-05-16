40eb3f37f286d6cf727cd5c5ac38f04fe1e499ee
Neil Spencer <neilhspencer@gmail.com>
2024-05-16 16:46:07 +0100

    Amendment to speed run time

--- tests/testthat/test-gehan.wilcoxon.R
+++ tests/testthat/test-gehan.wilcoxon.R
@@ -3,14 +3,12 @@ test_that("Example 9.1", {
                         ch9$asymp.censor, alternative = "less", seed = 1)
   expect_equal(tmp$pval.mc.stat, 217.5)
   expect_equal(tmp$pval.mc, 0.00372)
-  expect_equal(gehan.wilcoxon(ch9$symp.survtime, ch9$asymp.survtime,
-                              ch9$symp.censor, ch9$asymp.censor,
-                              alternative = "less", do.exact = FALSE,
-                              do.asymp = TRUE)$pval.asymp.stat, 217.5)
-  expect_equal(gehan.wilcoxon(ch9$symp.survtime, ch9$asymp.survtime,
-                              ch9$symp.censor, ch9$asymp.censor,
-                              alternative = "less", do.exact = FALSE,
-                              do.asymp = TRUE)$pval.asymp, 0.0050981517)
+  tmp <- gehan.wilcoxon(ch9$symp.survtime, ch9$asymp.survtime,
+                        ch9$symp.censor, ch9$asymp.censor,
+                        alternative = "less", do.exact = FALSE,
+                        do.asymp = TRUE)
+  expect_equal(tmp$pval.asymp.stat, 217.5)
+  expect_equal(tmp$pval.asymp, 0.0050981517)
 })
 
 test_that("Exercise 9.5", {
