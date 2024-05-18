5935c0f6b12a393fec0799e76be110421daa162c
Neil Spencer <neilhspencer@gmail.com>
2024-05-18 15:32:11 +0100

    Remove rounding from tests

--- tests/testthat/test-kruskal.wallis.vdW.R
+++ tests/testthat/test-kruskal.wallis.vdW.R
@@ -1,7 +1,7 @@
 test_that("Example 7.2", {
   tmp <- kruskal.wallis.vdW(ch7$age, ch7$positions)
   expect_equal(tmp$pval.exact.stat, 2.33260844)
-  expect_equal(round(tmp$pval.exact, 3), 0.336)
+  expect_equal(tmp$pval.exact, 0.33578921)
   expect_equal(
     kruskal.wallis.vdW(ch7$age, ch7$positions, do.exact = FALSE,
                        do.asymp = TRUE)$pval.asymp.stat, 2.33260844)
